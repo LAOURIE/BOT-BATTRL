ad7813b02334df73452c95b054358a92f2ec934e
LAOURIE <161684844+LAOURIE@users.noreply.github.com>
2024-05-07 12:30:21 +0300

    Delete CHALLENGE directory
    Unneccesary

--- CHALLENGE/solution-2.js
+++
@@ -1,9 +0,0 @@
-
-// function to calculate speed points
-const points = (speed) => {
-    if (speed === "") {
-        console.log("Value Input")
-    }
-    else if (speed < 70) {
-        console.log("Ok")
-    }
